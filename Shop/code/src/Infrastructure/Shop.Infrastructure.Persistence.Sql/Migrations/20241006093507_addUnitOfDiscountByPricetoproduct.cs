b2d1219e3a5d12c9021c86bd535134b4def0b39c
Ali Asgari <a.asgari@alibaba.ir>
2024-10-06 13:05:44 +0330

    adding migration

---
+++ Shop/code/src/Infrastructure/Shop.Infrastructure.Persistence.Sql/Migrations/20241006093507_addUnitOfDiscountByPricetoproduct.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Persistence.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUnitOfDiscountByPricetoproduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UnitOfDiscountByPrice",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Product_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Product_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "UnitOfDiscountByPrice",
+                table: "Product");
+        }
+    }
+}
